d7ca91acda9fba8ba3bd6f406a48a8239160158d
nnoitra05 <nexcoxin@gmail.com>
2022-11-06 06:26:28 +0900

    img-fluid追加

--- db/schema.rb
+++ db/schema.rb
@@ -50,10 +50,11 @@ ActiveRecord::Schema.define(version: 2022_10_31_162814) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "uid"
-    t.string "provider"
+    t.string "uid", null: false
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
